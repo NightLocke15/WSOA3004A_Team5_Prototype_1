f081237a84ec39931550f1c7d2b05fe0c9e96f21
2615283 <131881245+NightLocke15@users.noreply.github.com>
2024-07-22 23:29:49 +0200

    Movement fixed.
    Movement works as expected. Detects when player cube is on platform and when it is not.

--- Assets/Christine Scripts/Movement.cs
+++ Assets/Christine Scripts/Movement.cs
@@ -6,9 +6,34 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private float speed = 3;
     private bool _moving;
+    private float currentY;
+    [SerializeField] private float currentZRot;
+    [SerializeField] private float currentXRot;
+    [SerializeField] private bool upright = true;
+    [SerializeField] private bool left = false;
+    [SerializeField] private bool right = false;
+    [SerializeField] private bool front = true;
+    [SerializeField] private bool back = false;
+    public bool touchingFloor;
+    [SerializeField] private KeyCode lastKey;
+
+    [SerializeField] private List<string> strings = new List<string>();
 
     private void Update()
     {
+        currentY = transform.position.y;
+        currentZRot = transform.eulerAngles.z - 180;
+        currentXRot = transform.eulerAngles.x - 180;
+
+        if (currentY > 0.8f && currentY < 1.2f)
+        {
+            upright = true;
+        }
+        else
+        {
+            upright = false;
+        }
+
         if (_moving)
         {
             return;
@@ -16,31 +41,389 @@ public class Movement : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.A))
         {
-            Directions(Vector3.left);
-        }
+            if (upright == true)
+            {
+                var anchor = transform.position + (Vector3.down + (Vector3.left * 0.5f));
+                var axis = Vector3.Cross(Vector3.up, Vector3.left);
+                strings.Add("not upright");
 
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Directions(Vector3.forward);
-        }
+                StartCoroutine(Rolling(anchor, axis));
+            }
+            else if (upright == false)
+            {
 
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Directions(Vector3.back);
+                if (left == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.left);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.left);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.left) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.left);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+                else if (right == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.left);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.left);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.left) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.left);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+                else if (front == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.left) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.left);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.left);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.left);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+                else if (back == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.left) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.left);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.left);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.left);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+            }
+
+            left = true;
+            right = false;
+            front = false;
+            back = false;
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
-            Directions(Vector3.right);
+            if (upright == true)
+            {
+                var anchor = transform.position + (Vector3.down + (Vector3.right * 0.5f));
+                var axis = Vector3.Cross(Vector3.up, Vector3.right);
+                strings.Add("not upright");
+
+                StartCoroutine(Rolling(anchor, axis));
+            }
+            else if (upright == false)
+            {
+
+                if (left == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.right);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.right);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.right) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.right);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+                else if (right == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.right);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.right);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.right) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.right);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+                else if (front == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.right) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.right);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.right);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.right);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+                else if (back == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.right) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.right);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.right);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.right);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+            }
+
+            left = false;
+            right = true;
+            front = false;
+            back = false;
         }
 
-        void Directions (Vector3 direction)
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            var anchor = transform.position + (Vector3.down + direction) * 1f;
-            var axis = Vector3.Cross(Vector3.up, direction);
+            if (upright == true)
+            {
+                var anchor = transform.position + (Vector3.down + (Vector3.forward * 0.5f));
+                var axis = Vector3.Cross(Vector3.up, Vector3.forward);
+                strings.Add("not upright");
 
-            StartCoroutine(Rolling(anchor, axis));
+                StartCoroutine(Rolling(anchor, axis));
+            }
+            else if (upright == false)
+            {
+                if (left == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.forward) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.forward);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.forward);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.forward);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+                else if (right == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.forward) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.forward);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.forward);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.forward);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+                else if (front == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.forward);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.forward);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.forward) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.forward);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+                else if (back == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.forward);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.forward);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.forward) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.forward);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+            }
+
+            left = false;
+            right = false;
+            front = true;
+            back = false;
         }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (upright == true)
+            {
+                var anchor = transform.position + (Vector3.down + (Vector3.back * 0.5f));
+                var axis = Vector3.Cross(Vector3.up, Vector3.back);
+                strings.Add("not upright");
+
+                StartCoroutine(Rolling(anchor, axis));
+            }
+            else if (upright == false)
+            {
+                if (left == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.back) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.back);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.back);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.back);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+                else if (right == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.back) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.back);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.back);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.back);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+                else if (front == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.back);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.back);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.back) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.back);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+                else if (back == true)
+                {
+                    if (strings[strings.Count - 1] != strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + ((Vector3.down * 0.5f) + Vector3.back);
+                        var axis = Vector3.Cross(Vector3.up, Vector3.back);
+                        strings.Add("upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                    else if (strings[strings.Count - 1] == strings[strings.Count - 2])
+                    {
+                        var anchor = transform.position + (Vector3.down + Vector3.back) * 0.5f;
+                        var axis = Vector3.Cross(Vector3.up, Vector3.back);
+                        strings.Add("not upright");
+
+                        StartCoroutine(Rolling(anchor, axis));
+                    }
+                }
+            }
+
+            left = false;
+            right = false;
+            front = false;
+            back = true;
+        }     
     }
 
     IEnumerator Rolling(Vector3 anchor, Vector3 axis)
