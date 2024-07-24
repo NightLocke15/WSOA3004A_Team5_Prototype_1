a8ea0cf5f3ad16cc0075c268125328f102be656b
2615283 <131881245+NightLocke15@users.noreply.github.com>
2024-07-24 14:21:45 +0200

    Fixed Split Mod

--- Assets/Christine Scripts/Combination.cs
+++ Assets/Christine Scripts/Combination.cs
@@ -8,39 +8,49 @@ public class Combination : MonoBehaviour
     public GameObject cubeTwo;
     public GameObject playerCube;
 
-    public float positionZ;
-    public float positionX;
-
-
-    private void Update()
-    {
-        positionZ = (cubeOne.transform.position.z - cubeTwo.transform.position.z) / 2;
-        positionX = (cubeOne.transform.position.x - cubeTwo.transform.position.x) / 2;
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "HalfCube")
         {
-            if (cubeOne.transform.position.z < (cubeTwo.transform.position.z - 0.2) || cubeOne.transform.position.z > (cubeTwo.transform.position.z + 0.2)) // forward and back
+            if (cubeOne.transform.position.z < (cubeTwo.transform.position.z - 0.2)) // forward and back
             {
                 Debug.Log("front");
                 cubeOne.SetActive(false);
                 cubeTwo.SetActive(false);
                 playerCube.SetActive(true);
 
-                playerCube.transform.position = new Vector3(positionX, 0.5f, positionZ);
+                playerCube.transform.position = cubeOne.transform.position + new Vector3(0, 0, 0.5f);
                 playerCube.transform.rotation = Quaternion.Euler(90, 0, 0);
 
             }
-            else if (cubeOne.transform.position.x < (cubeTwo.transform.position.x - 0.2) || cubeOne.transform.position.x > (cubeTwo.transform.position.x + 0.2)) // left and right
+            else if (cubeOne.transform.position.z > (cubeTwo.transform.position.z + 0.2))
             {
                 Debug.Log("back");
                 cubeOne.SetActive(false);
                 cubeTwo.SetActive(false);
                 playerCube.SetActive(true);
 
-                playerCube.transform.position = new Vector3(positionX, 0.5f, positionZ);
+                playerCube.transform.position = cubeOne.transform.position + new Vector3(0, 0, -0.5f);
+                playerCube.transform.rotation = Quaternion.Euler(90, 0, 0);
+            }
+            else if (cubeOne.transform.position.x < (cubeTwo.transform.position.x - 0.2)) // left and right
+            {
+                Debug.Log("left");
+                cubeOne.SetActive(false);
+                cubeTwo.SetActive(false);
+                playerCube.SetActive(true);
+
+                playerCube.transform.position = cubeOne.transform.position + new Vector3(0.5f, 0, 0);
+                playerCube.transform.rotation = Quaternion.Euler(0, 0, 90);
+            }
+            else if (cubeOne.transform.position.x > (cubeTwo.transform.position.x + 0.2))
+            {
+                Debug.Log("right");
+                cubeOne.SetActive(false);
+                cubeTwo.SetActive(false);
+                playerCube.SetActive(true);
+
+                playerCube.transform.position = cubeOne.transform.position + new Vector3(-0.5f, 0, 0);
                 playerCube.transform.rotation = Quaternion.Euler(0, 0, 90);
             }
         }
